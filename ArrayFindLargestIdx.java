85dec4bda3db9ba272550bfecaa5ea5f801efc9a
Mani-Kanta-Reddy <ummadimanikanta@gmail.com>
2022-05-28 16:40:05 +0530

    Bit Manipulation

---
+++ ArrayFindLargestIdx.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import fastreader.FastReader;
+
+public class ArrayFindLargestIdx {
+	public static void main(String[] args) {
+		FastReader sc = new FastReader();
+		int[] arr = Arrays.stream(sc.nextLine().split("\\s"))
+						  .map(Integer::parseInt)
+						  .mapToInt(Integer::valueOf)
+						  .toArray();
+		// System.out.println(Arrays.toString(arr));
+		System.out.println(naive(arr)); //O(n ^ 2)
+		System.out.println(findIdxOfLargest(arr));
+		
+	}
+	static int naive(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			boolean flag = true; //assume arr[i] is largest
+			for(int j = 0; j < arr.length; j++) { //check if someone is larger than arr[i]
+				if(arr[j] > arr[i]) {
+					flag = false;
+					break;
+				}
+			}
+			if(flag == true) {
+				return i;
+			}
+		}
+		return -1;	//this line is never reached but should return something to make compiler happy
+	}
+	static int findIdxOfLargest(int[] arr) {
+		//assume arr[0] is largest and start:
+		int idxOfLargest = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > arr[idxOfLargest]) idxOfLargest = i;
+		}
+		return idxOfLargest;
+	}
+}
