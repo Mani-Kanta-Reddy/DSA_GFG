5c35d2e6caf178e42a56c0913e5cabde02ff5e78
Mani-Kanta-Reddy <ummadimanikanta@gmail.com>
2023-05-09 14:15:22 +0530

    small changes

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
-import java.util.Arrays;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        String java_home = System.getenv("M2_HOME");
-        System.out.println(java_home);
-        
-    
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println((3 & 1) == 0);
     }
 }
+
 
